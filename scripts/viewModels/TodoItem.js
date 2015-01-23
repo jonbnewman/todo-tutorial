3e37ad2416d15eefea668d5d4f722c6a4372b56a
Jonathan Newman <jonathan@overfoc.us>
2015-01-22 20:32:23 -0600

    can now complete a todo entry

--- scripts/viewModels/TodoItem.js
+++ scripts/viewModels/TodoItem.js
@@ -5,6 +5,14 @@ define(['footwork'],
       initialize: function(thingToDo) {
         // store the string text entered by user as thingToDo
         this.thingToDo = fw.observable(thingToDo);
+
+        // stores the state of the TodoItem (true = done, false = not done)
+        this.isDone = fw.observable(false);
+
+        // toggle the state of this TodoItem
+        this.toggleDone = function() {
+          this.isDone(!this.isDone());
+        };
       }
     });
   }
